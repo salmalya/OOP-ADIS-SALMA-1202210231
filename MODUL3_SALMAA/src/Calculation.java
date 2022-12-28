4ed72191bdb8c36600c59dd57d4c2acae03fb03b
salmalya <116442098+salmalya@users.noreply.github.com>
2022-12-28 15:30:01 +0700

    Add files via upload

---
+++ MODUL3_SALMAA/src/Calculation.java
@@ -0,0 +1,21 @@
+public class Calculation{
+    private double radius;
+    private double side;
+    private double area;
+    static double phi = 3.14;
+
+
+public void SetSquare(int side){
+    try{
+        double luas = side*side;
+        this.side = side;
+    }catch(InterruptedException e){
+     //   e.printStackTrace();
+    }
+}
+public double setCircle(){
+    return luas(area);
+    }
+    
+}
+// enggak tau lagi kakk TnT
---
+++ MODUL3_SALMAA/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main {
+    private boolean repeat = true;
+
+    public static void main(String[] args) throws Exception {
+        System.out.println();
+    }
+}
